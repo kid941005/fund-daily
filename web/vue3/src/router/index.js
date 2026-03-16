9169b96879ac10faab7ecca530065a839c20e9ab
kid <1919888301@qq.com>
2026-03-16 14:25:32 +0800

    optimize: 路由懒加载
    - 每个页面独立打包 (Home, Holdings, Analysis, Quant)
    - 首屏加载进一步优化

--- web/vue3/src/router/index.js
+++ web/vue3/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
-import Holdings from '@/views/Holdings.vue'
-import Analysis from '@/views/Analysis.vue'
-import Quant from '@/views/Quant.vue'
+
+// 路由懒加载
+const Home = () => import('@/views/Home.vue')
+const Holdings = () => import('@/views/Holdings.vue')
+const Analysis = () => import('@/views/Analysis.vue')
+const Quant = () => import('@/views/Quant.vue')
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
